c89a004c2c4886effc33c551a20598dc55cdb749
Winston Lin <winstonnlinn@gmail.com>
2025-06-12 00:02:07 -0400

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
             <section id = "home" style={{ textAlign: 'center', padding: '2rem' }}>
                 {/* <h1> Hello, Winston here... </h1> */}
                 <Intro />
-                <p> 
+                <p className = "text-3xl font-bold underline"> 
                     your boba fueled Developer from nyc... 
                     trying to document the struggle of becoming a Software Engineer..
                 </p>
