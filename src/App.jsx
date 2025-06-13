2789fa5a6103933b8574c0eb67a81006a85bea47
Mayday46 <winstonnlinn@gmail.com>
2025-06-12 23:16:58 -0400

    Refactor App and main components to integrate ThemeProvider and improve structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import About from './page/About.jsx'
-import Contact from './page/Contact';
 import Footer from './components/Footer';
-import Home from './page/Home';
 import NavigationBar from './components/NavigationBar';
+import { ThemeProvider } from './context/ThemeContext.jsx';
+import About from './page/About.jsx';
+import Contact from './page/Contact';
+import Home from './page/Home';
 import Project from './page/Project';
 
 function App() {
   return (
-    <div className = "app">
-      <NavigationBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Project />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </div>
+
+    // <div className = "app">
+    //   <NavigationBar />
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/about" element={<About />} />
+    //     <Route path="/projects" element={<Project />} />
+    //     <Route path="/contact" element={<Contact />} />
+    //   </Routes>
+    //   <Footer />
+    // </div>
+
+    <ThemeProvider>
+      <div className="app">
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Project />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
+import { ThemeProvider } from './context/ThemeContext.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
-  <Router>
-    <App />
-  </Router>
+  // <Router>
+  //   <App />
+  // </Router>
+  <React.StrictMode>
+    <ThemeProvider>
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
+  </React.StrictMode>
 );
