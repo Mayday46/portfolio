ed47972ccef108e9a9cb2af38efa19105eda4bca
Mayday46 <winstonnlinn@gmail.com>
2025-06-20 23:22:20 -0400

    refactor: replace motion elements in About component with Intro component for improved content management

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -1,17 +1,15 @@
 
-import { motion } from "framer-motion";
+import Intro from "../components/Intro.jsx";
 import { FadeInSection } from "../utils/FadeInSection";
 
 const About = () => {
     return (
         <div>
             <FadeInSection>
-                <motion.h1 className = "text-3xl font-bold">
-                    This is who I am.
-                </motion.h1>
-                <motion.p className = "text-sm sm:text-base leading-relaxed mb-6">
-                    Part-time Debugger, Full-Time Boba Enthusiast
-                </motion.p>
+                <Intro
+                    heading = "This is who I am..."
+                    subheading = "writing code, sipping tea, and figuring things out along the way."
+                />
             </FadeInSection>
         </div>
     );
