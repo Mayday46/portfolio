0a710cde9b564fa28a12e055bfe046f343684216
Mayday46 <winstonnlinn@gmail.com>
2025-06-23 10:17:50 -0400

    feat: add new photo entry for 'Spring 2024' in photos data

--- src/data/photos.jsx
+++ src/data/photos.jsx
@@ -1,5 +1,6 @@
 
 import me1 from '../assets/me1.jpg';
+import me2 from '../assets/me2.jpg';
 
 
 const photos = [
@@ -9,6 +10,14 @@ const photos = [
         location: "NYC Long Island Strawberry Farm",
         caption: "Picking strawberrys for boba",
         style: "sm:top-4 sm:left-[-10%] top-2 left-2 rotate-[-6deg] z-10"
+    },
+
+    {
+        image: me2,
+        date: 'Spring 2024',
+        location: 'New York Japan',
+        caption: 'pretenting to be in Japan',
+        style: "sm:top-4 sm:left-[10%] top-2 right-2 rotate-[6deg] z-20"
     }
 ]
 
