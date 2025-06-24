52164290c5494538cc7b8df4e700fd70c20ca9ca
Mayday46 <winstonnlinn@gmail.com>
2025-06-24 19:14:30 -0400

    feat: add new images and entries for 'me3' and 'me4' in photos data

--- src/data/photos.jsx
+++ src/data/photos.jsx
@@ -1,6 +1,8 @@
 
 import me1 from '../assets/me1.jpg';
 import me2 from '../assets/me2.jpg';
+import me3 from '../assets/me3.jpg';
+import me4 from '../assets/me4.jpg';
 
 
 const photos = [
@@ -17,8 +19,26 @@ const photos = [
         date: 'Spring 2024',
         location: 'New York Japan',
         caption: 'pretenting to be in Japan',
-        style: "sm:top-4 sm:left-[10%] top-2 right-2 rotate-[6deg] z-20"
+        style: "sm:top-12 sm:left-[15%] top-8 right-2 rotate-[4deg] z-20"
+    },
+
+    {
+        image: me3,
+        date: '3/29/2025',
+        location: 'Allegiant Stadium Las Vegas',
+        caption: '5525 Mayday Concert',
+        style: "sm:left-[45%] top-8 right-8 rotate-[-3deg] z-30"
+    },
+
+    {
+        image: me4,
+        date: '3/28/2025',
+        location: 'Allegiant Stadium Las Vegas',
+        caption: '5525 Mayday Concert',
+        style: "top-[10%] left-[70%] rotate-[5deg] z-40"
     }
+
+
 ]
 
 
